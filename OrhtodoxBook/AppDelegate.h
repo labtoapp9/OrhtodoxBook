8eb2571f3f1058694acd49df5c546563c5a0df83
Shadyk <Apple@shadyk-mac.local>
2014-06-15 21:13:09 +0300

    Initial Commit

---
+++ OrhtodoxBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OrhtodoxBook
+//
+//  Created by Shadyk on 6/15/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OrhtodoxBook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OrhtodoxBook
+//
+//  Created by Shadyk on 6/15/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
